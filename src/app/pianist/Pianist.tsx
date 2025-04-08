1dfbf79b9c0f2f30b8d0181d6eec36d1deb6a0b1
Guillermo Martin <gscalica@gmail.com>
2025-04-07 18:40:32 -0700

    Add 'hero', 'audio', and 'performances' sections with basic content on 'Pianist' page

--- src/app/pianist/Pianist.tsx
+++ src/app/pianist/Pianist.tsx
@@ -1,7 +1,65 @@
+import Image from "next/image";
+
 function Pianist() {
   return (
     <div>
-      <h1>Pianist page</h1>
+      {/* ---------- Hero section ---------- */}
+      <div className="hero-section">
+        {/* ----- Text container ----- */}
+        <div className="text-container">
+          <h1>Pianist page</h1>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam nobis esse est. Error sed alias earum ipsum officiis labore veritatis minima repellat tempore laborum eligendi nostrum, laudantium facere aperiam illum.</p>
+        </div>
+        
+        {/* ----- Image container ----- */}
+        <div className="image-container">
+          <Image src="/images/placeholder-1.jpg" alt="Placeholer image" width={680} height={428} />
+        </div>
+      </div>
+
+      {/* ---------- Audio section ---------- */}
+      <div className="audio-section">
+        {/* ----- Text container ----- */}
+        <div>
+          <h2>Audio</h2>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas maxime accusantium ex eaque quaerat, et, hic tempora corporis eius libero neque sint tenetur excepturi quod, consequuntur nihil perferendis sapiente necessitatibus.</p>
+        </div>
+
+        {/* ----- Audio container ----- */}
+        <div>
+          <ul>
+            <li>
+              <h3>Title</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores iste quas mollitia maxime quam, iure in eligendi perspiciatis eos eveniet aperiam quibusdam facere alias veritatis facilis amet? Nulla, omnis architecto.</p>
+              <audio controls src={null}></audio>
+            </li>
+            <li>
+              <h3>Title</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores iste quas mollitia maxime quam, iure in eligendi perspiciatis eos eveniet aperiam quibusdam facere alias veritatis facilis amet? Nulla, omnis architecto.</p>
+              <audio controls src={null}></audio>
+            </li>
+            <li>
+              <h3>Title</h3>
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores iste quas mollitia maxime quam, iure in eligendi perspiciatis eos eveniet aperiam quibusdam facere alias veritatis facilis amet? Nulla, omnis architecto.</p>
+              <audio controls src={null}></audio>
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      {/* ---------- Performances section ---------- */}
+      <div className="performances-section">
+        {/* ----- Text container ----- */}
+        <div>
+          <h2>Performances</h2>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas maxime accusantium ex eaque quaerat, et, hic tempora corporis eius libero neque sint tenetur excepturi quod, consequuntur nihil perferendis sapiente necessitatibus.</p>
+        </div>
+
+        {/* ----- Videos container ----- */}
+        <div>
+          
+        </div>
+      </div>
     </div>
   );
 };
