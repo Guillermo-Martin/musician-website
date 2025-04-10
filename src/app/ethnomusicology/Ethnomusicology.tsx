5a56a6aed84dee04b7e502fa8ec9fd5fadcdc602
Guillermo Martin <gscalica@gmail.com>
2025-04-10 16:39:13 -0700

    Add basic content to 'Ethnomusicology' page

--- src/app/ethnomusicology/Ethnomusicology.tsx
+++ src/app/ethnomusicology/Ethnomusicology.tsx
@@ -1,7 +1,97 @@
+import Image from "next/image";
+
 function Ethnomusicology() {
   return (
     <div>
-      <h1>Ethnomusicology page</h1>
+      {/* ---------- Hero section ---------- */}
+      <div className="hero-section">
+        {/* ----- Text container ----- */}
+        <div className="text-container">
+          <h1>Ethnomusicology page</h1>
+          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam nobis esse est. Error sed alias earum ipsum officiis labore veritatis minima repellat tempore laborum eligendi nostrum, laudantium facere aperiam illum.</p>
+        </div>
+        
+        {/* ----- Image container ----- */}
+        <div className="image-container">
+          <Image src="/images/placeholder-1.jpg" alt="Placeholer image" width={680} height={428} />
+        </div>
+      </div>
+
+      {/* ---------- Abstracts ---------- */}
+      <div className="abstracts">
+        <h2>Abstracts</h2>
+
+        {/* ----- Abstract ----- */}
+        <div>
+          <h3>Abstract title</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nam, veniam maxime voluptate eaque debitis aspernatur dolor minus a porro. Excepturi quaerat vel accusamus neque asperiores veritatis praesentium distinctio harum?</p>
+        </div>
+        <div>
+          <h3>Abstract title</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nam, veniam maxime voluptate eaque debitis aspernatur dolor minus a porro. Excepturi quaerat vel accusamus neque asperiores veritatis praesentium distinctio harum?</p>
+        </div>
+        <div>
+          <h3>Abstract title</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nam, veniam maxime voluptate eaque debitis aspernatur dolor minus a porro. Excepturi quaerat vel accusamus neque asperiores veritatis praesentium distinctio harum?</p>
+        </div>
+      </div>
+
+      {/* ---------- Publications ---------- */}
+      <div className="publications">
+        <h2>Publications</h2>
+
+        {/* ----- Publication ----- */}
+        <div>
+          <h3>Publication title</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nam, veniam maxime voluptate eaque debitis aspernatur dolor minus a porro. Excepturi quaerat vel accusamus neque asperiores veritatis praesentium distinctio harum?</p>
+        </div>
+        <div>
+          <h3>Publication title</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nam, veniam maxime voluptate eaque debitis aspernatur dolor minus a porro. Excepturi quaerat vel accusamus neque asperiores veritatis praesentium distinctio harum?</p>
+        </div>
+        <div>
+          <h3>Publication title</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nam, veniam maxime voluptate eaque debitis aspernatur dolor minus a porro. Excepturi quaerat vel accusamus neque asperiores veritatis praesentium distinctio harum?</p>
+        </div>
+      </div>
+
+      {/* ---------- Conference papers ---------- */}
+      <div className="conference-papers">
+        <h2>Conference papers</h2>
+
+        {/* ----- Conference paper ----- */}
+        <div>
+          <h3>Conference paper title</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nam, veniam maxime voluptate eaque debitis aspernatur dolor minus a porro. Excepturi quaerat vel accusamus neque asperiores veritatis praesentium distinctio harum?</p>
+        </div>
+        <div>
+          <h3>Conference paper title</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nam, veniam maxime voluptate eaque debitis aspernatur dolor minus a porro. Excepturi quaerat vel accusamus neque asperiores veritatis praesentium distinctio harum?</p>
+        </div>
+        <div>
+          <h3>Conference paper title</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nam, veniam maxime voluptate eaque debitis aspernatur dolor minus a porro. Excepturi quaerat vel accusamus neque asperiores veritatis praesentium distinctio harum?</p>
+        </div>
+      </div>
+
+      {/* ---------- Courses taught ---------- */}
+      <div className="courses">
+        <h2>Courses taught</h2>
+
+        {/* ----- Course taught ----- */}
+        <div>
+          <h3>Course taught title</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nam, veniam maxime voluptate eaque debitis aspernatur dolor minus a porro. Excepturi quaerat vel accusamus neque asperiores veritatis praesentium distinctio harum?</p>
+        </div>
+        <div>
+          <h3>Course taught title</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nam, veniam maxime voluptate eaque debitis aspernatur dolor minus a porro. Excepturi quaerat vel accusamus neque asperiores veritatis praesentium distinctio harum?</p>
+        </div>
+        <div>
+          <h3>Course taught title</h3>
+          <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius nam, veniam maxime voluptate eaque debitis aspernatur dolor minus a porro. Excepturi quaerat vel accusamus neque asperiores veritatis praesentium distinctio harum?</p>
+        </div>
+      </div>
     </div>
   );
 };
