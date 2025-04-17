58dcdf40c004ba159142f014f4ded3c92f86fb13
Guillermo Martin <gscalica@gmail.com>
2025-04-16 17:20:40 -0700

    Create Jest tests for 'Hero' component

---
+++ __tests__/Hero.test.tsx
@@ -0,0 +1,90 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
+import Hero from "@/components/Hero";
+
+describe("Hero", () => {
+  // Before running each test, render the "Hero" component with props passed in
+  // ----- Arrange -----
+  beforeEach(() => {
+    render(
+      <Hero 
+        pageTitle="This is the page title"
+        hasDescription={true}
+        description="This is a short description about the page."
+        src="./images/placeholder-1.jpg"
+        alt="Placeholder image"
+      />
+    );
+  });
+
+  // ---------- it should display an h1 ----------
+  it("Renders an H1", () => {
+    // ----- Action -----
+    // get the h1
+    const heading = screen.getByRole("heading", { level: 1 });
+
+    // ----- Assert -----
+    // expect the h1 to show up and isn't empty
+    expect(heading).toBeInTheDocument();
+    expect(heading).not.toBe("");
+  });
+
+  // ---------- it should display a description ----------
+  it("Renders a short description of the page", () => {
+    // ----- Action -----
+    const pageDescription = screen.getByTestId("page-description");
+  
+    // ----- Assert -----
+    // expect that the page description is present and isn't empty
+    expect(pageDescription).toBeInTheDocument();
+    expect(pageDescription).not.toBe("");
+  });
+
+  // ---------- it should display an image ----------
+  it("Renders an image with an alt tag", () => {
+    // ----- Action -----
+    const heroImage = screen.getByRole("img");
+
+    // * expect the image component shows up
+    expect(heroImage).toBeInTheDocument();
+    
+    // * expect that the src is there and isn't empty
+    expect(heroImage).toHaveAttribute("src");
+    expect(heroImage.getAttribute("src")).not.toBe("");
+
+    // * expect that the alt tag isn't empty
+    expect(heroImage).toHaveAttribute("alt");
+    expect(heroImage.getAttribute("alt")).not.toBe("");
+    });
+
+  // ---------- it should only display an image if "hasDescription is false" ----------
+  it("Only renders a title and image, if there's no description", () => {
+    // ----- Arrange -----
+    render(
+      <Hero 
+        pageTitle="This is the page title"
+        hasDescription={false}
+        description="This is a short description about the page."
+        src="./images/placeholder-1.jpg"
+        alt="Placeholder image"
+      />
+    );
+
+    // ----- Action -----
+    // get the page description
+    const pageDescription = screen.getByTestId("page-description");
+
+    // ----- Assert -----
+    // expect the description to not be rendered
+    expect(pageDescription).not.toBeInTheDocument()
+  });
+});
+
+// to dos
+// make sure to pass in props to the component
+// make sure that a string is passed in to the text elements and it's not empty
+// look up beforeEach()
