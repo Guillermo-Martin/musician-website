d3b2bf7b64b97eb6b009b8c82806377597ecad83
Guillermo Martin <gscalica@gmail.com>
2025-04-28 17:27:48 -0700

    Create basic schema types (for practice) for Sanity studio

---
+++ src/sanity/schemaTypes/artistType.ts
@@ -0,0 +1,14 @@
+// ---------- EXAMPLE SCHEMA ----------
+import {defineField, defineType} from 'sanity'
+
+export const artistType = defineType({
+  name: 'artist',
+  title: 'Artist',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'name',
+      type: 'string',
+    }),
+  ],
+})
--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,6 +1,10 @@
 import { type SchemaTypeDefinition } from 'sanity'
 import { postType } from "./postType";
+import { eventType } from "./eventType";
+import { artistType } from './artistType';
+import { venueType } from './venueType';
 
+// https://www.sanity.io/learn/course/day-one-with-sanity-studio/getting-started
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [postType],
+  types: [postType, eventType, artistType, venueType],
 }
