9eed65db5304f98b94504ef7c285ccb9cf723b51
Guillermo Martin <gscalica@gmail.com>
2025-05-13 14:45:46 -0700

    Create basic schemas for 'About', 'Compositions', 'Contact', 'Ethnomusicology', 'Events', and 'Pianist' pages

---
+++ src/sanity/schemaTypes/eventsType.ts
@@ -0,0 +1,34 @@
+import { defineField, defineType } from "sanity";
+
+export const eventsType = defineType({
+  name: "eventsPage",
+  title: "Events",
+  type: "document",
+  fields: [
+    defineField({
+      name: "page_title",
+      type: "string",
+    }),
+    defineField({
+      name: "slug",
+      title: "Slug",
+      type: "slug",
+      options: {source: "page_title"},
+      validation: (rule) => rule
+        .required()
+        .error(`Required to generate a page on the website`),
+    }),
+    defineField({
+      name: "short_description",
+      type: "string",
+    }),
+    // * the defineField below is for being able to query all pages to make links
+    defineField({
+      name: "pageType",
+      type: "string",
+      readOnly: true,
+      hidden: true,
+      initialValue: "page"
+    })
+  ],
+});
--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -5,8 +5,13 @@ import { artistType } from './artistType';
 import { venueType } from './venueType';
 import { homeType } from "./homeType";
 import { aboutType } from "./aboutType";
+import { compositionsType } from './compositionsType';
+import { contactType } from './contactType';
+import { ethnomusicologyType } from './ethnomusicologyType';
+import { eventsType } from './eventsType';
+import { pianistType } from "./pianistType";
 
 // https://www.sanity.io/learn/course/day-one-with-sanity-studio/getting-started
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [postType, eventType, artistType, venueType, homeType, aboutType],
+  types: [postType, eventType, artistType, venueType, homeType, aboutType, compositionsType, contactType, ethnomusicologyType, eventsType, pianistType],
 }
