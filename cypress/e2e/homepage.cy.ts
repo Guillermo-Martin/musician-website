4e3b4a03bac9c4edcf4d61cd91072ce34e8e5470
Guillermo Martin <gscalica@gmail.com>
2025-05-14 18:09:11 -0700

    Add Cypress test to check for navbar links on homepage

--- cypress/e2e/homepage.cy.ts
+++ cypress/e2e/homepage.cy.ts
@@ -3,11 +3,12 @@ describe("Homepage", () => {
     // arrange: visit homepage
     cy.visit("/");
 
-    // action: find the elements (h1, subheader, image, audio)
+    // action: find the elements (h1, subheader, image, audio, navbar links)
     const h1 = cy.get("h1");
     const subHeader = cy.get(".subheader");
     const img = cy.get("img");
     const audio = cy.get("audio");
+    const navbarLinks = cy.get(".navbar a");
 
     // assert:
     // * the h1 has a title
@@ -21,5 +22,10 @@ describe("Homepage", () => {
 
     // * the audio has a src
     audio.should("be.visible").and("have.attr", "src").and("not.be.empty");
+
+    // * the navbar has links that aren't empty and the href has a path to the correct page
+    navbarLinks.each(link => {
+      cy.wrap(link).should("be.visible").and("not.be.empty").and("have.attr", "href").and("not.be.empty");
+    });
   });
 });
