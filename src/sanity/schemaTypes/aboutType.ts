11fb4ea2391f6f3d04d6945b5a7d9b5992ad7e8c
Guillermo Martin <gscalica@gmail.com>
2025-06-16 16:45:51 -0700

    Create schema for 'About' page

--- src/sanity/schemaTypes/aboutType.ts
+++ src/sanity/schemaTypes/aboutType.ts
@@ -21,8 +21,162 @@ export const aboutType = defineType({
       //   .error(`Required to generate a page on the website`),
     }),
     defineField({
-      name: "short_description",
+      name: "hero_image",
+      type: "image",
+      fields: [
+        {
+          name: "alt_text",
+          type: "string"
+        }
+      ]
+    }),
+    defineField({
+      name: "subsection1_title",
       type: "string",
+    }),
+    defineField({
+      name: "subsection1_description",
+      type: "text",
+    }),
+    defineField({
+      name: "subsection1_image",
+      type: "image",
+      fields: [
+        {
+          name: "alt_text",
+          type: "string"
+        }
+      ]
+    }),
+    defineField({
+      name: "subsection2_title",
+      type: "string",
+    }),
+    defineField({
+      name: "subsection2_description",
+      type: "text",
+    }),
+    defineField({
+      name: "subsection2_image",
+      type: "image",
+      fields: [
+        {
+          name: "alt_text",
+          type: "string"
+        }
+      ]
+    }),
+    defineField({
+      name: "subsection3_title",
+      type: "string",
+    }),
+    defineField({
+      name: "subsection3_description",
+      type: "text",
+    }),
+    defineField({
+      name: "quote_image",
+      type: "image",
+      fields: [
+        {
+          name: "alt_text",
+          type: "string"
+        }
+      ]
+    }),
+    defineField({
+      name: "quote",
+      type: "text",
+    }),
+    defineField({
+      name: "gallery_image1",
+      type: "image",
+      fields: [
+        {
+          name: "alt_text",
+          type: "string"
+        }
+      ]
+    }),
+    defineField({
+      name: "gallery_image2",
+      type: "image",
+      fields: [
+        {
+          name: "alt_text",
+          type: "string"
+        }
+      ]
+    }),
+    defineField({
+      name: "gallery_image3",
+      type: "image",
+      fields: [
+        {
+          name: "alt_text",
+          type: "string"
+        }
+      ]
+    }),
+    defineField({
+      name: "gallery_image4",
+      type: "image",
+      fields: [
+        {
+          name: "alt_text",
+          type: "string"
+        }
+      ]
+    }),
+    defineField({
+      name: "gallery_image5",
+      type: "image",
+      fields: [
+        {
+          name: "alt_text",
+          type: "string"
+        }
+      ]
+    }),
+    defineField({
+      name: "gallery_image6",
+      type: "image",
+      fields: [
+        {
+          name: "alt_text",
+          type: "string"
+        }
+      ]
+    }),
+    defineField({
+      name: "gallery_image7",
+      type: "image",
+      fields: [
+        {
+          name: "alt_text",
+          type: "string"
+        }
+      ]
+    }),
+    defineField({
+      name: "gallery_image8",
+      type: "image",
+      fields: [
+        {
+          name: "alt_text",
+          type: "string"
+        }
+      ]
+    }),
+    defineField({
+      name: "gallery_image9",
+      type: "image",
+      fields: [
+        {
+          name: "alt_text",
+          type: "string"
+        }
+      ]
     }),
     // * the defineField below is for being able to query all pages to make links
     defineField({
