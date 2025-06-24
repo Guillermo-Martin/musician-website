765d493fea56ab3a154c1450e7290d20cb6bc820
Guillermo Martin <gscalica@gmail.com>
2025-06-23 17:16:06 -0700

    Create 'compositionsType' Sanity schema

--- src/sanity/schemaTypes/compositionsType.ts
+++ src/sanity/schemaTypes/compositionsType.ts
@@ -24,6 +24,78 @@ export const compositionsType = defineType({
       name: "short_description",
       type: "string",
     }),
+    defineField({
+      name: "hero_image",
+      type: "image",
+      fields: [
+        {
+          name: "alt_text",
+          type: "string"
+        }
+      ]
+    }),
+    defineField({
+      name: "subsection1_title",
+      type: "string",
+    }),
+    defineField({
+      name: "subsection1_description",
+      type: "text",
+    }),
+    defineField({
+      name: "song1_title",
+      title: "Song #1 title",
+      type: "string",
+    }),
+    defineField({
+      name: "song1_date",
+      title: "Song #1 date",
+      type: "string",
+    }),
+    defineField({
+      name: "song1_audio_file",
+      title: "Song #1 audio file",
+      type: "file",
+      options: {
+        accept: "audio/*"
+      }
+    }),
+    defineField({
+      name: "song2_title",
+      title: "Song #2 title",
+      type: "string",
+    }),
+    defineField({
+      name: "song2_date",
+      title: "Song #2 date",
+      type: "string",
+    }),
+    defineField({
+      name: "song2_audio_file",
+      title: "Song #2 audio file",
+      type: "file",
+      options: {
+        accept: "audio/*"
+      }
+    }),
+    defineField({
+      name: "song3_title",
+      title: "Song #3 title",
+      type: "string",
+    }),
+    defineField({
+      name: "song3_date",
+      title: "Song #3 date",
+      type: "string",
+    }),
+    defineField({
+      name: "song3_audio_file",
+      title: "Song #3 audio file",
+      type: "file",
+      options: {
+        accept: "audio/*"
+      }
+    }),
     // * the defineField below is for being able to query all pages to make links
     defineField({
       name: "pageType",
