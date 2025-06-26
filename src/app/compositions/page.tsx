7a1050d91031a86642749f32c1480f9a665a7155
Guillermo Martin <gscalica@gmail.com>
2025-06-25 17:47:03 -0700

    Add query to 'Compositions' page to query data and pass as props to 'Compositions' component

--- src/app/compositions/page.tsx
+++ src/app/compositions/page.tsx
@@ -1,3 +1,5 @@
+import { defineQuery } from "next-sanity";
+import { sanityFetch } from "@/sanity/lib/live";
 import type { Metadata } from "next";
 import Compositions from "./Compositions"
 
@@ -7,9 +9,22 @@ export const metadata: Metadata = {
   description: "Compositions."
 }
 
+// ---------- Query to Sanity ----------
+const COMPOSITIONS_PAGE_QUERY = defineQuery(`{
+  'compositionsPage': *[_type == 'compositionsPage' && !(_id in path('drafts.**'))]{
+    'heroText': {page_title, short_description},
+    'heroImage': {hero_image {asset -> {url}, alt_text}},
+    'subsectionText': {subsection1_title, subsection1_description},
+    "works": works[]{title, audio_file {asset -> {url}}}
+  }
+}`);
+
 // ---------- Page ----------
-function CompositionsPage() {
-  return <Compositions />
+async function CompositionsPage() {
+  // query Sanity to get data and pass as props to "Compositions" component
+  const { data } = await sanityFetch({ query: COMPOSITIONS_PAGE_QUERY });
+
+  return <Compositions data={data}/>
 };
 
 export default CompositionsPage;
