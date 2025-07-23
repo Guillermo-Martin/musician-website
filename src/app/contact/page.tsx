54f488a6b9870c6ac423c094619c6d4dd42a4a61
Guillermo Martin <gscalica@gmail.com>
2025-07-23 16:22:47 -0700

    Create GROQ query for 'Contact' page and fetch data

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,5 @@
+import { defineQuery } from "next-sanity";
+import { sanityFetch } from "@/sanity/lib/live";
 import type { Metadata } from "next";
 import Contact from "./Contact";
 
@@ -7,8 +9,22 @@ export const metadata: Metadata = {
   description: "Contact"
 }
 
+
+// ---------- Query to Sanity ----------
+const CONTACT_PAGE_QUERY = defineQuery(`{
+  'contactPage': *[_type == 'contactPage' && !(_id in path('drafts.**'))]{
+    'heroText': {page_title, short_description},
+    'socials': socials[]{social_media, href}
+  }
+}`);
+
 // ---------- Page ----------
-function ContactPage() {
+async function ContactPage() {
+  // query Sanity to get data and pass as props to "Contact" component
+  const { data } = await sanityFetch({ query: CONTACT_PAGE_QUERY });
+
+  console.log("here's your data for contacts", data);
+
   return <Contact />
 };
 
