55069aa9f751e7422f342f491806dd5c2e375562
Guillermo Martin <gscalica@gmail.com>
2025-10-15 18:16:35 -0700

    Add schema for 'Events' page

--- src/sanity/schemaTypes/eventsType.ts
+++ src/sanity/schemaTypes/eventsType.ts
@@ -24,6 +24,59 @@ export const eventsType = defineType({
       name: "short_description",
       type: "string",
     }),
+    defineField({
+      name: "hero_image",
+      title: "Hero image",
+      type: "image",
+      fields: [
+        {
+          name: "alt_text",
+          type: "string"
+        }
+      ]
+    }),
+    // array type:  https://www.sanity.io/docs/studio/array-type
+    defineField({
+      name: "upcoming_events",
+      title: "Upcoming events",
+      type: "array",
+      of: [
+        {
+          type: "object",
+          fields: [
+            {
+              name: 'date',
+              title: 'Date',
+              type: 'date',
+              options: {
+                dateFormat: 'MM-DD-YYYY',
+              }
+            },
+            {
+              name: 'time',
+              title: 'Time',
+              type: 'string'
+            },
+            {
+              name: 'location',
+              title: 'Location',
+              type: 'string'
+            },
+            {
+              name: 'description',
+              title: 'Description',
+              type: 'text'
+            },
+            {
+              name: 'link',
+              title: 'Where to buy tickets (if applicable)',
+              type: 'url',
+              description: 'Enter the URL where people can buy tickets.  Otherwise, leave this blank.'
+            }
+          ]
+        }
+      ]
+    }),
     // * the defineField below is for being able to query all pages to make links
     defineField({
       name: "pageType",
