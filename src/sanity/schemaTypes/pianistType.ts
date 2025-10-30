623eddf210ded33e022a29108d5da321525cf9bd
Guillermo Martin <gscalica@gmail.com>
2025-10-29 18:10:48 -0700

    Create 'Pianist' page Sanity schema

--- src/sanity/schemaTypes/pianistType.ts
+++ src/sanity/schemaTypes/pianistType.ts
@@ -24,6 +24,91 @@ export const pianistType = defineType({
       name: "short_description",
       type: "string",
     }),
+    defineField({
+      name: "hero_image",
+      title: "Hero image",
+      type: "image",
+      fields: [
+        {
+          name: "alt_text",
+          type: "string"
+        }
+      ]
+    }),
+    // array type:  https://www.sanity.io/docs/studio/array-type
+    defineField({
+      name: "performances",
+      title: "Performances",
+      description: "Add the links to your YouTube videos here.",
+      type: "array",
+      of: [
+        {
+          type: "object",
+          fields: [
+            {
+              name: 'title',
+              title: 'Title',
+              type: 'string'
+            },
+            {
+              name: 'youtube_share_link',
+              title: 'YouTube share link',
+              type: 'url'
+            },
+          ]
+        }
+      ]
+    }),
+    defineField({
+      name: "cds",
+      title: "CDs",
+      description: "Add your CDs here.",
+      type: "array",
+      of: [
+        {
+          type: "object",
+          fields: [
+            {
+              name: 'title',
+              title: 'Title',
+              type: 'string'
+            },
+            {
+              name: 'cd_image',
+              title: 'CD image',
+              type: 'image'
+            },
+            {
+              name: "songs",
+              title: "Songs",
+              type: "array",
+              of: [
+                {
+                  type: "object",
+                  title: "Song",
+                  fields: [
+                    {
+                      name: "title",
+                      title: "Title",
+                      type: "string",
+                    },
+                    {
+                      name: "audio",
+                      title: "Audio",
+                      type: "file",
+                      options: {
+                        accept: "audio/*"
+                      }
+                    }
+                  ]
+                }
+              ]
+            }
+          ]
+        },
+      ]
+    }),
+    
     // * the defineField below is for being able to query all pages to make links
     defineField({
       name: "pageType",
