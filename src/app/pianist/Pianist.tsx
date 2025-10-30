5cfaadb26277919a47894eb28f45771175e84f97
Guillermo Martin <gscalica@gmail.com>
2025-10-30 16:48:47 -0700

    Render data on 'Pianist' page

--- src/app/pianist/Pianist.tsx
+++ src/app/pianist/Pianist.tsx
@@ -49,27 +49,55 @@ interface PianistProps {
 
 
 function Pianist({ data }: PianistProps) {
-  console.log("in Pianist page", data)
+  // console.log("in Pianist page", data.pianistPage[0].cds)
+  
+  // destructure hero information
+  const { heroImage, heroText, performances, cds } = data.pianistPage[0];
 
-  // const allVideos = videoLinks.map(link => {
-  //   // get the youtube video id from the share link
-  //   // 1. split the link by "https://youtu.be/"
-  //   //    - you'll get an array that looks like: ["https://youtu.be/", "1yfxp1NhLIs?si=TedHUzcuviVe9pM-"]
-  //   // 2. next, get the second half of the link (which is index 1)
-  //   // 3. split that string by "?".
-  //   //    - you'll get an array that looks like: ["1yfxp1NhLIs", "si=TedHUzcuviVe9pM-"]
-  //   //    - the string at index 0 is the video id
-  //   const youTubeVideoId = link.split("https://youtu.be/")[1].split("?")[0]
+  // for each YouTube video, create an iframe
+  const allVideos = performances.map(performance => {
+    // get the youtube video id from the share link
+    // 1. split the link by "https://youtu.be/"
+    //    - you'll get an array that looks like: ["https://youtu.be/", "1yfxp1NhLIs?si=TedHUzcuviVe9pM-"]
+    // 2. next, get the second half of the link (which is index 1)
+    // 3. split that string by "?".
+    //    - you'll get an array that looks like: ["1yfxp1NhLIs", "si=TedHUzcuviVe9pM-"]
+    //    - the string at index 0 is the video id
+    const youTubeVideoId = performance.youtube_share_link.split("https://youtu.be/")[1].split("?")[0]
 
-  //   // create the youtube embed link
-  //   // YouTube embed url for iframe: https://developers.google.com/youtube/player_parameters#:~:text=AI%2Dgenerated%20Key%20Takeaways,listType%20parameters%20in%20the%20URL.
-  //   let youTubeEmbedLink = `https://www.youtube.com/embed/${youTubeVideoId}`
+    // create the youtube embed link
+    // YouTube embed url for iframe: https://developers.google.com/youtube/player_parameters#:~:text=AI%2Dgenerated%20Key%20Takeaways,listType%20parameters%20in%20the%20URL.
+    let youTubeEmbedLink = `https://www.youtube.com/embed/${youTubeVideoId}`
 
-  //   // add the youTubeEmbedLink to the youtube's iframe
-  //   return (
-  //     <iframe width="560" height="315" src={youTubeEmbedLink} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-  //   );
-  // });
+    // add the youTubeEmbedLink to the youtube's iframe
+    return (
+      <div key={performance._key}>
+        <h3>{performance.title}</h3>
+        <iframe width="560" height="315" src={youTubeEmbedLink} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+      </div>
+    );
+  });
+
+  // for each cd, create a "cd info section" with the title, image, and songs
+  const allCds = cds.map(cd => {
+    // for each song in a cd, create an audio player
+    const cdSongs = cd.songs.map(song => {
+      return (
+        <div>
+          <h6>{song.title}</h6>
+          <audio controls src={song.audio.asset.url}></audio>
+        </div>
+      );
+    });
+
+    return (
+      <div>
+        <Image src={cd.cd_image.asset.url} alt="Placeholer image" width={680} height={428} />
+        <h5>{cd.title}</h5>
+        <div>{cdSongs}</div>
+      </div>
+    );
+  });
 
 
   return (
@@ -78,17 +106,14 @@ function Pianist({ data }: PianistProps) {
       <div className="hero-section">
         {/* ----- Text container ----- */}
         <div className="text-container">
-          <h1>Pianist page</h1>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Aperiam nobis esse est. Error sed alias earum ipsum officiis labore veritatis minima repellat tempore laborum eligendi nostrum, laudantium facere aperiam illum.</p>
+          <h1>{heroText.page_title}</h1>
+          <p>{heroText.short_description}</p>
         </div>
         
         {/* ----- Image container ----- */}
         <div className="image-container">
-          <Image src="/images/placeholder-1.jpg" alt="Placeholer image" width={680} height={428} />
+          <Image src={heroImage.hero_image.asset.url} alt={heroImage.hero_image.alt_text} width={680} height={428} />
         </div>
-
-        {/* ----- Youtube videos ----- */}
-        {/* <div>{allVideos}</div> */}
       </div>
 
       {/* ---------- Audio section ---------- */}
@@ -101,7 +126,7 @@ function Pianist({ data }: PianistProps) {
 
         {/* ----- Audio container ----- */}
         <div>
-          <ul>
+          {/* <ul>
             <li>
               <h3>Title</h3>
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores iste quas mollitia maxime quam, iure in eligendi perspiciatis eos eveniet aperiam quibusdam facere alias veritatis facilis amet? Nulla, omnis architecto.</p>
@@ -117,7 +142,8 @@ function Pianist({ data }: PianistProps) {
               <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores iste quas mollitia maxime quam, iure in eligendi perspiciatis eos eveniet aperiam quibusdam facere alias veritatis facilis amet? Nulla, omnis architecto.</p>
               <audio controls src={null}></audio>
             </li>
-          </ul>
+          </ul> */}
+          {allCds}
         </div>
       </div>
 
@@ -131,7 +157,7 @@ function Pianist({ data }: PianistProps) {
 
         {/* ----- Videos container ----- */}
         <div>
-          <div>
+          {/* <div>
             <p>Video title</p>
             <Image src="/images/placeholder-1.jpg" alt="Placeholer image" width={680} height={428} />
           </div>
@@ -146,7 +172,9 @@ function Pianist({ data }: PianistProps) {
           <div>
             <p>Video title</p>
             <Image src="/images/placeholder-1.jpg" alt="Placeholer image" width={680} height={428} />
-          </div>
+          </div> */}
+          {/* ----- Youtube videos ----- */}
+          <div>{allVideos}</div>
         </div>
       </div>
 
