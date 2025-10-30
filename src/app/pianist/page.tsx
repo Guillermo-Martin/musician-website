7bd84cf1c7d9a708ef3ba8c4e9d7aefee65acb42
Guillermo Martin <gscalica@gmail.com>
2025-10-29 18:25:34 -0700

    Query Sanity and get data for 'Pianist' page

--- src/app/pianist/page.tsx
+++ src/app/pianist/page.tsx
@@ -1,3 +1,5 @@
+import { defineQuery } from "next-sanity";
+import { sanityFetch } from "@/sanity/lib/live";
 import type { Metadata } from "next";
 import Pianist from "./Pianist";
 
@@ -7,8 +9,23 @@ export const metadata: Metadata = {
   description: "Pianist"
 }
 
+// ---------- Query to Sanity ----------
+const PIANIST_PAGE_QUERY = defineQuery(`{
+  'pianistPage': *[_type == 'pianistPage' && !(_id in path('drafts.**'))]{
+    'heroText': {page_title, short_description},
+    'heroImage': {hero_image {asset -> {url}, alt_text}},
+    'performances': performances[]{_key, title, youtube_share_link},
+    'cds': cds[]{_key, title, cd_image, songs}
+  }
+}`);
+
 // ---------- Page ----------
-function PianistPage() {
+async function PianistPage() {
+  // query Sanity to get data and pass props to "Pianist" component
+  const { data } = await sanityFetch({ query: PIANIST_PAGE_QUERY });
+
+  console.log("pianist data", data)
+
   return <Pianist />
 };
 
